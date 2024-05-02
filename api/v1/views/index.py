2ed2f463884dd70514fd43e3b56e866531e5815f
Evans Manyala <evans.manyala@gmail.com>
2024-05-02 04:38:19 +0300

    Addin more code logic

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,17 @@
 """Creating and Index"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    status_data = {"status": "OK"}
-    return jsonify(status_data)
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
